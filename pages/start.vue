62fd8bc731ff70ee16dace4eac351292ecdb3f9e
tudou0316 <154646457+tudou0316@users.noreply.github.com>
2024-05-08 18:41:13 +0800

    前端over
    那个拼图没写，AR也没有

--- pages/start.vue
+++ pages/start.vue
@@ -1,16 +1,14 @@
 <template>
 	<view class="bgc">
-		<view class="bgc-circle">
-			<image src="../static/首页/circle.png" mode="" style="width: 770rpx;height: 870rpx;"></image>
-		</view>
-		<view class="icon-container">
-			<!-- 遍历一个包含图标信息的数组来动态生成图标和文字 -->
-			<view v-for="(icon, index) in icons" :key="index" class="imgIcon" :style="getIconStyle(index)"
-				@click="switchToTabPage(icon.pagePath)">
-				<view :class="{ 'circle': index !== 2, 'feiyi-circle': index === 2 }">
-					<image class="icon" :class="{ 'feiyi-icon': index === 2 }" :src="icon.src" />
+
+
+		<view class="img">
+			<image src="../static/首页/circle-bgc.jpg" mode="aspectFit" class="image"></image>
+			<view class="icon-container">
+				<view v-for="(icon, index) in icons" :key="index" class="icon-box"
+					:class="{ 'special-box': index === 2 }" :style="getIconStyle(index)"
+					@click="switchToTabPage(icon.pagePath)">
 				</view>
-				<text class="icon-label" :class="{ 'feiyi-label': index === 2 }">{{ icon.label }}</text>
 			</view>
 		</view>
 		<view class="bottom">
@@ -24,56 +22,46 @@
 	export default {
 		data() {
 			return {
-				// 图标数据，包含图标路径、文字和对应跳转页面的路径
-				icons: [{
-						src: '../static/首页/keCheng.png',
-						label: '趣学课程',
+				icons: [
+					// 假设这是您的图标数据，现在改为五个盒子的数据
+					{
 						pagePath: 'Lesson/Lesson'
 					},
 					{
-						src: '../static/首页/zixun.png',
-						label: '趣学资讯',
 						pagePath: 'Consult/Consult'
 					},
 					{
-						src: '../static/首页/feiYi.png',
-						label: '趣学非遗',
 						pagePath: 'index/index'
 					},
 					{
-						src: '../static/首页/tiYan.png',
-						label: '趣学体验',
 						pagePath: 'experience/experience'
 					},
 					{
-						src: '../static/首页/gengDuo.png',
-						label: '趣学更多',
 						pagePath: 'stillmore/stillmore'
 					}
 				]
 			};
 		},
 		methods: {
-			// 动态获取每个图标的位置样式
 			getIconStyle(index) {
 				const positions = [{
-						top: '220rpx',
-						right: '240rpx'
-					},
-					{
-						top: '480rpx',
-						left: '50rpx'
-					},
-					{
-						top: '450rpx',
+						bottom: '860rpx',
 						right: '230rpx'
 					},
 					{
-						top: '540rpx',
+						bottom: '600rpx',
+						left: '50rpx'
+					},
+					{
+						bottom: '530rpx',
+						right: '230rpx'
+					},
+					{
+						bottom: '530rpx',
 						right: '10rpx'
 					},
 					{
-						top: '790rpx',
+						bottom: '300rpx',
 						left: '200rpx'
 					}
 				];
@@ -96,73 +84,43 @@
 		background-size: cover;
 		height: 100%;
 		width: 100%;
-		position: absolute;
+	}
+
+	.img {
+		height: 80vh;
+		position: relative;
+		top: -100rpx;
+	}
+
+	.image {
+		width: 100%;
+		height: 100%;
+
 	}
 
 	.icon-container {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		align-items: center;
-		padding: 10px;
+		position: relative;
 	}
 
-	.imgIcon {
+	.icon-box {
 		position: absolute;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
+		width: 150rpx;
+		height: 150rpx;
+		background-color: rgba(0, 0, 0, 0);
 	}
-
-	.circle {
-		width: 70px;
-		height: 65px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border-radius: 50%;
-		margin-bottom: 5px;
-	}
-
-	.icon {
-		width: 50px;
-		height: 45px;
-		margin-bottom: 5px;
-		border-radius: 50%;
-	}
-
-	.feiyi-icon {
-		width: 250rpx;
+	.special-box {
+		width: 230rpx;
 		height: 240rpx;
-		background-color: transparent;
 	}
 
-	.feiyi-circle {
-		background-color: transparent;
-	}
+	
 
 	@font-face {
 		font-family: 'CustomFont';
 		src: url('../static/fonts/XiangJiaoDaJiangJunLingGanTi-2.ttf') format('truetype');
 	}
 
-	.icon-label {
-		font-family: 'CustomFont';
-		font-size: 20px;
-		color: #696a60;
-		line-height: 40px;
-
-	}
-
-	/* 单独为趣学非遗文字设置样式 */
-	.feiyi-label {
-		font-size: 25px;
-		color: black;
-		font-weight: normal;
-	}
-
+	
 	.bottom {
 		position: absolute;
 		bottom: 0;
--- pages/stillmore/Communication.vue
+++ pages/stillmore/Communication.vue
@@ -6,18 +6,19 @@
 				<text>{{head.username}}</text>
 			</view>
 			<text style="color: aliceblue;font-family: 'MyCustomFont';font-size: 40rpx;">{{head.title}}</text>
-			<text style="color: aliceblue;font-family: 'MyCustomFont';font-size: 20rpx;font-weight: 100;">{{head.text}}</text>
+			<text
+				style="color: aliceblue;font-family: 'MyCustomFont';font-size: 20rpx;font-weight: 100;">{{head.text}}</text>
 		</view>
 		<text style="font-family: 'MyCustomFontBold';font-size: 40rpx;">评论</text>
 		<view class="discussion">
-		    <view class="comment" v-for="(item, index) in comment" :key="index">
-		      <image class="avatar" :src="item.avatar"  />
-		      <view class="message-content">
-		        <text class="username">{{ item.username }}</text>
-		        <text class="text" >{{ item.text }}</text>
-		      </view>
-		    </view>
-		  </view>
+			<view class="comment" v-for="(item, index) in comment" :key="index">
+				<image class="avatar" :src="item.avatar" />
+				<view class="message-content">
+					<text class="username">{{ item.username }}</text>
+					<text class="text">{{ item.text }}</text>
+				</view>
+			</view>
+		</view>
 	</view>
 
 </template>
@@ -32,18 +33,17 @@
 					title: '先去桂林还是阳朔?让三姐带你攻略桂林最美旅途风光!',
 					text: '桂林最值得看的就是象鼻山~但现在的象鼻山都是旅行团,嘈杂非常步道两侧都是低劣小商品,实在扫兴桂林作为一个老牌旅游城市,景点管理太差了除了象鼻山,桂林的滨江路和解放桥还可以走走其他就没啥好看的了大家想看象鼻山的话看我的图就行,没必要专门跑到桂林去看,当然这只是我的看法,大家看有啥更好的推荐敬请评论区下方留言~'
 				},
-				comment: [
-				        {
-				          username: '阿牛哥',
-				          avatar: '/static/页面4/aniuguo_.png', 
-				          text: '象鼻山是在阳朔还是桂林？'
-				        },
-				        {
-				          username: '爱旅游的小趴菜儿',
-				          avatar: '/static/页面4/lanyangyang.png', 
-				          text: '姐妹，我想问一下，遇龙河也是在兴坪古镇附近么？还是在阳朔县？我现在在桂林不知道是坐车到阳朔站还是兴坪古镇？'
-				        }
-				      ]
+				comment: [{
+						username: '阿牛哥',
+						avatar: '/static/页面4/aniuguo_.png',
+						text: '象鼻山是在阳朔还是桂林？'
+					},
+					{
+						username: '爱旅游的小趴菜儿',
+						avatar: '/static/页面4/lanyangyang.png',
+						text: '姐妹，我想问一下，遇龙河也是在兴坪古镇附近么？还是在阳朔县？我现在在桂林不知道是坐车到阳朔站还是兴坪古镇?'
+					}
+				]
 			}
 		},
 		methods: {
@@ -68,11 +68,13 @@
 		flex-direction: column;
 		align-items: flex-start;
 	}
-	.avatarhead{
+
+	.avatarhead {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
 	}
+
 	.avatarhead text {
 		font-family: 'MyCustomFont';
 		font-size: 40rpx;
@@ -84,36 +86,45 @@
 		width: 150rpx;
 		height: 150rpx;
 		padding: 25rpx;
+		overflow: hidden;
 	}
-	.discussion{
+
+	.discussion {
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
-		
+
 	}
-	.comment{
+
+	.comment {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
 		width: 700rpx;
-		height: 250rpx;
+		height: auto;
 		margin-bottom: 20rpx;
 		background-color: #e5d7ca;
 		border-radius: 15rpx;
 	}
-	.message-content{
+
+	.message-content {
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
-		margin-left: 50rpx;
+		margin-left: 20rpx;
+		white-space: normal;
+		overflow: hidden;
+		flex: 1;
 	}
-	.username{
+
+	.username {
 		font-family: 'MyCustomFontBold';
 		font-weight: bold;
 		margin-top: 20rpx;
-		
+
 	}
-	.message-content text{
+
+	.message-content text {
 		font-family: 'MyCustomFont';
 		margin-top: 20rpx;
 
